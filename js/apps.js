2ff464dc14d4184d8eb94f0b1291efcd9f7c51a9
boburbekburkhonov <zafaroviccc@gmail.com>
2022-06-26 16:12:10 +0500

    feat: JavaScript

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,157 @@
-const elHeaderLike = document.querySelector(".like");
+"use strict"
 
+const elHeaderLike = document.querySelector(".like");
+const elCategories = document.querySelector(".categories__list");
+const elProducts = document.querySelector(".products__list");
+const elProductsAll = document.querySelector(".products__top-desc-blue");
+const elForm = document.querySelector(".header__form");
+const elSelect = document.querySelector(".header__select");
+const elInput = document.querySelector(".header__input");
+const elbookmark = document.querySelector(".bookmark");
 
 elHeaderLike.addEventListener("click", () => {
   elHeaderLike.classList.toggle("header__like")
+})
+
+elbookmark.addEventListener("click", () => {
+  console.log("dsd");
+})
+
+const renderSelect = function(item){
+  const uniqueSelect = [];
+
+  item.forEach(categors => {
+    if(!uniqueSelect.includes(categors.categories)){
+      uniqueSelect.push(categors.categories)
+    }
+  })
+
+  uniqueSelect.forEach(element => {
+    const newOption = document.createElement("option");
+
+    newOption.textContent = element;
+
+    elSelect.appendChild(newOption)
+  })
+}
+
+renderSelect(products)
+
+const renderProducts = function(prods, element){
+  prods.forEach(item => {
+    const newProdsItem = document.createElement("li");
+    const newProdsImg = document.createElement("img");
+    const newProdsDiv = document.createElement("div");
+    const newProdsHeading = document.createElement("h3");
+    const newProdsTitle = document.createElement("p");
+    const newProdsPrice = document.createElement("p");
+    const newProdsBtn = document.createElement("a");
+    const newProdsBtnView = document.createElement("a");
+
+    newProdsItem.style.width = "18rem";
+    newProdsItem.style.marginBottom = "2rem";
+    newProdsItem.setAttribute("class", "card products__item")
+    newProdsImg.setAttribute("src", item.poster);
+    newProdsImg.setAttribute("class", "card-img-top");
+    newProdsDiv.setAttribute("class", "card-body");
+    newProdsHeading.setAttribute("class", "card-title");
+    newProdsTitle.setAttribute("class", "card-text");
+    newProdsTitle.style.fontWeight = "600"
+    newProdsTitle.style.fontSize = "20px";
+    newProdsTitle.style.color = "#FF9900"
+    newProdsPrice.setAttribute("class", "card-text");
+    newProdsPrice.style.fontWeight = "600"
+    newProdsPrice.style.fontSize = "20px"
+    newProdsPrice.style.color = "dodgerblue";
+    newProdsBtn.setAttribute("class", "btn btn-primary")
+    newProdsBtnView.setAttribute("class", "btn btn-outline-primary ms-3")
+
+    newProdsHeading.textContent = item.categories;
+    newProdsTitle.textContent = item.title;
+    newProdsPrice.textContent = item.price;
+    newProdsBtn.textContent = "Add to Cart"
+    newProdsBtnView.textContent = "Quick view"
+
+    element.appendChild(newProdsItem);
+    newProdsItem.appendChild(newProdsImg);
+    newProdsItem.appendChild(newProdsDiv);
+    newProdsDiv.appendChild(newProdsHeading);
+    newProdsDiv.appendChild(newProdsTitle);
+    newProdsDiv.appendChild(newProdsPrice);
+    newProdsDiv.appendChild(newProdsBtn);
+    newProdsDiv.appendChild(newProdsBtnView);
+  })
+}
+
+const renderCategories = function(categors, htmlElement){
+  const unique = [];
+
+  categors.forEach(item => {
+    if(!unique.includes(item.categories)){
+      unique.push(item.categories)
+    }
+  })
+
+  unique.forEach(item => {
+    const newCategoriesItem = document.createElement("li");
+
+    newCategoriesItem.textContent = item;
+
+    newCategoriesItem.classList.add("categories__item")
+
+    htmlElement.appendChild(newCategoriesItem);
+  })
+}
+
+renderCategories(products, elCategories);
+renderProducts(products, elProducts);
+
+elCategories.addEventListener("click", (evt) => {
+  const foundCategories = evt.target.textContent;
+
+  const filterCategories = products.filter(item => item.categories === foundCategories);
+
+  elProducts.innerHTML = null;
+
+  renderProducts(filterCategories, elProducts);
+})
+
+elProductsAll.addEventListener("click", () => {
+  elProducts.innerHTML = null;
+
+  renderProducts(products, elProducts)
+})
+
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  if(elInput.value){
+    const inputValue = elInput.value[0].toUpperCase() + elInput.value.slice(1).toLowerCase();
+    elInput.value = null;
+
+    const filteredInputValue = products.filter(item => item.categories === inputValue);
+
+    elProducts.innerHTML = null;
+
+    renderProducts(filteredInputValue, elProducts);
+
+  } else if(elSelect.value){
+    const selectValue = elSelect.value;
+
+    const filterSelectValue = [];
+
+    products.forEach(item => {
+      if(item.categories.includes(selectValue)){
+        filterSelectValue.push(item);
+      }
+    })
+
+    elProducts.innerHTML = null;
+
+   renderProducts(filterSelectValue, elProducts);
+
+   if(selectValue === "all"){
+    renderProducts(products, elProducts);
+   }
+  }
 })
---
+++ js/products.js
@@ -0,0 +1,161 @@
+const products = [
+  {
+    id: "1",
+    title: "Samsung Android A32",
+    poster: "http://bitly.ws/srBr",
+    price: "$300",
+    categories: "Phone"
+  },
+
+  {
+    id: "2",
+    title: "Samsung Android A52",
+    poster: "http://bitly.ws/srBt",
+    price: "$350",
+    categories: "Phone"
+  },
+
+  {
+    id: "3",
+    title: "Xiomi Redmi Note 7",
+    poster: "http://bitly.ws/srBw",
+    price: "$220",
+    categories: "Phone"
+  },
+
+  {
+    id: "4",
+    title: "Apple Iphone 13 Pro",
+    poster: "http://bitly.ws/srBx",
+    price: "$1000",
+    categories: "Phone"
+  },
+
+  {
+    id: "12",
+    title: "Apple Iphone 13 Pro Max",
+    poster: "http://bitly.ws/srBY",
+    price: "$1200",
+    categories: "Phone"
+  },
+
+  {
+    id: "5",
+    title: "Noutbook HP",
+    poster: "http://bitly.ws/srBE",
+    price: "$700",
+    categories: "Noutbook"
+  },
+
+  {
+    id: "6",
+    title: "Noutbook Lenova",
+    poster: "http://bitly.ws/srBH",
+    price: "$700",
+    categories: "Noutbook"
+  },
+
+  {
+    id: "7",
+    title: "Noutbook MAcbook",
+    poster: "http://bitly.ws/srBJ",
+    price: "$2000",
+    categories: "Noutbook"
+  },
+
+  {
+    id: "8",
+    title: "Smart Watches",
+    poster: "http://bitly.ws/srBR",
+    price: "$500",
+    categories: "Watch"
+  },
+
+  {
+    id: "9",
+    title: "Smart Watches",
+    poster: "http://bitly.ws/srBU",
+    price: "$550",
+    categories: "Watch"
+  },
+
+  {
+    id: "10",
+    title: "Smart Watches",
+    poster: "http://bitly.ws/srBX",
+    price: "$300",
+    categories: "Watch"
+  },
+
+  {
+    id: "11",
+    title: "Novey Black",
+    poster: "http://bitly.ws/srC3",
+    price: "$400",
+    categories: "Phone"
+  },
+
+  {
+    id: "13",
+    title: "Konditsaner Artel Everest12",
+    poster: "http://bitly.ws/srC6",
+    price: "$300",
+    categories: "Konditsaner"
+  },
+
+  {
+    id: "14",
+    title: "Logitech C922",
+    poster: "http://bitly.ws/srCc",
+    price: "$100",
+    categories: "Veb-kamera"
+  },
+
+  {
+    id: "15",
+    title: "Logitech HD Pro Webcam C922",
+    poster: "http://bitly.ws/srCf",
+    price: "$100",
+    categories: "Veb-kamera"
+  },
+
+  {
+    id: "16",
+    title: "Buxgalteriya",
+    poster: "http://bitly.ws/srCm",
+    price: "$8",
+    categories: "Biznes-book"
+  },
+
+  {
+    id: "17",
+    title: "Stephen R.Covey",
+    poster: "http://bitly.ws/srCr",
+    price: "$7",
+    categories: "Biznes-book"
+  },
+
+  {
+    id: "18",
+    title: "Liders",
+    poster: "http://bitly.ws/srCt",
+    price: "$10",
+    categories: "Biznes-book"
+  },
+
+  {
+    id: "19",
+    title: "Children clothes",
+    poster: "http://bitly.ws/srCy",
+    price: "130 000 so'm",
+    categories: "Clothes"
+  },
+
+  {
+    id: "20",
+    title: "Children clothes",
+    poster: "http://bitly.ws/srCA",
+    price: "130 000 so'm",
+    categories: "Clothes"
+  },
+]
