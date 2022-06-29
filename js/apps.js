2e9fad3e716b724da40a40e7bf901cf261a0af7c
boburbekburkhonov <zafaroviccc@gmail.com>
2022-06-29 13:30:38 +0500

    feat: modal

--- js/apps.js
+++ js/apps.js
@@ -7,14 +7,17 @@ const elProductsAll = document.querySelector(".products__top-desc-blue");
 const elForm = document.querySelector(".header__form");
 const elSelect = document.querySelector(".header__select");
 const elInput = document.querySelector(".header__input");
-const elbookmark = document.querySelector(".bookmark");
+const elModal = document.querySelector(".modal");
+const elBookmark = document.querySelector(".block");
+const elOverlay = document.querySelector(".overlay");
+const elCloseBtn = document.querySelector(".close-modal");
+
+const localStorage = JSON.parse(window.localStorage.getItem("bookmark"))
+
+const bookmark = localStorage || []
 
 elHeaderLike.addEventListener("click", () => {
   elHeaderLike.classList.toggle("header__like")
-})
-
-elbookmark.addEventListener("click", () => {
-  console.log("dsd");
 })
 
 const renderSelect = function(item){
@@ -70,6 +73,8 @@ const renderProducts = function(prods, element){
     newProdsTitle.textContent = item.title;
     newProdsPrice.textContent = item.price;
     newProdsBtn.textContent = "Add to Cart"
+    newProdsBtn.classList.add("bookmark-btn");
+    newProdsBtn.dataset.bookmarkId = item.id;
     newProdsBtnView.textContent = "Quick view"
 
     element.appendChild(newProdsItem);
@@ -122,6 +127,7 @@ elProductsAll.addEventListener("click", () => {
   renderProducts(products, elProducts)
 })
 
+
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
 
@@ -153,5 +159,137 @@ elForm.addEventListener("submit", (evt) => {
    if(selectValue === "all"){
     renderProducts(products, elProducts);
    }
+  }
+})
+
+const renderBookmark = function(array, html) {
+  array.forEach(item => {
+    const newCloseBtn = document.createElement("button");
+    const newProdsItem = document.createElement("li");
+    const newProdsImg = document.createElement("img");
+    const newProdsDiv = document.createElement("div");
+    const newProdsHeading = document.createElement("h3");
+    const newProdsTitle = document.createElement("p");
+    const newProdsPrice = document.createElement("p");
+    const newProdsBtn = document.createElement("a");
+    const newProdsBtnView = document.createElement("a");
+    const newProdsRemove = document.createElement("button");
+
+    newCloseBtn.classList.add("close-modal")
+
+    newProdsItem.style.width = "18rem";
+    newProdsItem.style.marginBottom = "2rem";
+    newProdsItem.setAttribute("class", "card products__item")
+    newProdsImg.setAttribute("src", item.poster);
+    newProdsImg.setAttribute("class", "card-img-top");
+    newProdsDiv.setAttribute("class", "card-body");
+    newProdsHeading.setAttribute("class", "card-title");
+    newProdsTitle.setAttribute("class", "card-text");
+    newProdsTitle.style.fontWeight = "600"
+    newProdsTitle.style.fontSize = "20px";
+    newProdsTitle.style.color = "#FF9900"
+    newProdsPrice.setAttribute("class", "card-text");
+    newProdsPrice.style.fontWeight = "600"
+    newProdsPrice.style.fontSize = "20px"
+    newProdsPrice.style.color = "dodgerblue";
+    newProdsBtn.setAttribute("class", "btn btn-primary")
+    newProdsBtnView.setAttribute("class", "btn btn-outline-primary ms-3");
+    newProdsRemove.setAttribute("class", "btn btn-danger ms-5 mt-3")
+
+    newProdsHeading.textContent = item.categories;
+    newProdsTitle.textContent = item.title;
+    newProdsPrice.textContent = item.price;
+    newProdsBtn.textContent = "Add to Cart"
+    newProdsBtn.classList.add("bookmark-btn");
+    newProdsBtn.dataset.bookmarkId = item.id;
+    newProdsBtnView.textContent = "Quick view";
+    newProdsRemove.textContent = "Remove"
+
+    newProdsRemove.dataset.removeBookmark = item.id;
+
+    html.appendChild(newProdsItem);
+    html.appendChild(newCloseBtn);
+    newProdsItem.appendChild(newProdsImg);
+    newProdsItem.appendChild(newProdsDiv);
+    newProdsDiv.appendChild(newProdsHeading);
+    newProdsDiv.appendChild(newProdsTitle);
+    newProdsDiv.appendChild(newProdsPrice);
+    newProdsDiv.appendChild(newProdsBtn);
+    newProdsDiv.appendChild(newProdsBtnView);
+    newProdsDiv.appendChild(newProdsRemove);
+  })
+}
+
+renderBookmark(bookmark, elModal)
+
+elProducts.addEventListener("click", (evt) => {
+  if(evt.target.matches(".bookmark-btn")){
+    const foundBookmark = evt.target.dataset.bookmarkId;
+    const findBookmark = products.find(item => item.id === foundBookmark);
+
+    if(!bookmark.includes(findBookmark)){
+      bookmark.push(findBookmark)
+    }
+
+
+
+    if(bookmark.length > 0){
+      elBookmark.classList.add("active")
+    }
+
+    elModal.innerHTML = null;
+
+    window.localStorage.setItem("bookmark", JSON.stringify(bookmark));
+
+    renderBookmark(bookmark, elModal)
+  }
+})
+
+
+function open() {
+  elModal.classList.remove("d-none");
+  elOverlay.classList.remove("d-none")
+}
+
+function close() {
+  elModal.classList.add("d-none");
+  elOverlay.classList.add("d-none")
+}
+
+elBookmark.addEventListener("click", open);
+
+elOverlay.addEventListener("click", close);
+
+elModal.addEventListener("click", (evt) => {
+  if(evt.target.matches(".close-modal")){
+    close()
+  }
+})
+
+document.addEventListener("keydown", (evt) => {
+  if(evt.keyCode === 27){
+    if(!elModal.classList.contains("d-none")){
+      close()
+    }
+  }
+})
+
+elModal.addEventListener("click", (evt) => {
+  if(evt.target.matches(".btn-danger")){
+    const bookmarRemoveId = evt.target.dataset.removeBookmark;
+    const foundBookmarkRemove = bookmark.findIndex(item => item.id === bookmarRemoveId);
+
+    elModal.innerHTML = null;
+
+    bookmark.splice(foundBookmarkRemove, 1);
+
+    window.localStorage.setItem("bookmark", JSON.stringify(bookmark))
+
+    if(bookmark.length === 0){
+      elBookmark.classList.remove("active");
+      window.localStorage.removeItem("bookmark")
+    }
+
+    renderBookmark(bookmark, elModal)
   }
 })
